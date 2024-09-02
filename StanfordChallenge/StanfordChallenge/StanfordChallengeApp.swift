032c8c64920e7c07c2a3e4690e405e7d4480ce66
Neel <realneelarora@gmail.com>
2024-09-02 11:02:40 -0700

    Stanford assignment

---
+++ StanfordChallenge/StanfordChallenge/StanfordChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordChallengeApp.swift
+//  StanfordChallenge
+//
+//  Created by Neel Arora on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct StanfordChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
